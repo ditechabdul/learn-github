b25623e0106734a5ccdcb52054bb8ec200fb9538
manan786 <malicksahab4@gmail.com>
2022-11-16 23:48:14 +0500

    New changes in index.js

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 import React from "react";
 
 const index = () => {
-  return <div>Abdul Manan</div>;
+  return (
+    <section>
+      <div className="hello">
+        <div className="col-md-12">
+          <div className="row">
+            <div className="col-4">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem
+              ipsa quas eos voluptas at doloribus corporis, nisi laudantium.
+              Voluptate libero laudantium reprehenderit eaque, aspernatur
+              impedit repellat vero architecto necessitatibus in!
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default index;
