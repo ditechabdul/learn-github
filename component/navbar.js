dc0962179432b755fbb23f5bc2cbc1dd7b7450c4
ditechabdul <ditechabdul@gmail.com>
2022-11-17 15:39:59 +0500

    add new navbar

---
+++ component/navbar.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const navbar = () => {
+  return (
+    <ul>
+      <li>
+        <a href="http://"></a>
+      </li>
+      <li>
+        <a href="http://"></a>
+      </li>
+      <li>
+        <a href="http://"></a>
+      </li>
+      <li>
+        <a href="http://"></a>
+      </li>
+      <li>
+        <a href="http://"></a>
+      </li>
+    </ul>
+  );
+};
+
+export default navbar;
