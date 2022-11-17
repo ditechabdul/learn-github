80da48b6a03540ac75e17fbcdd137b1996288d79
manan786 <malicksahab4@gmail.com>
2022-11-17 14:52:25 +0500

    add changes in app.js file

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
 import React from "react";
 
 const app = () => {
-  return <div>app</div>;
+  return (
+    <div className="app-test-class">
+      <div className="row">
+        <div className="col-md-12">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis
+          temporibus, accusantium aliquam mollitia quia laborum perferendis
+          magni tempora vero rerum ullam enim. Quisquam quos saepe laudantium
+          doloremque doloribus natus molestiae excepturi, delectus quasi
+          similique? Repellendus dolor ducimus sed sequi quidem pariatur
+          doloribus ut excepturi ea neque at facilis distinctio quis impedit
+          nihil, corporis iure quam rem beatae iusto officiis, perferendis
+          consequatur voluptatibus reiciendis! Porro corporis similique aperiam,
+          fugit natus dicta ratione numquam earum cupiditate sequi illo magni
+          eaque, sed labore a fuga quibusdam ea itaque repudiandae illum quas
+          delectus blanditiis ipsum. In dolore expedita animi libero deserunt
+          cumque tempore aliquam porro commodi dolores obcaecati odit nihil,
+          corrupti harum sint odio! Suscipit corporis ipsum, accusamus quae
+          dolores minus quisquam facilis voluptates.
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default app;
