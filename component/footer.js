622c487ff0b41f6727666e184dd241bd6652746d
ditechabdul <ditechabdul@gmail.com>
2022-11-21 14:21:35 +0500

    add footer.js file in component

---
+++ component/footer.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const navbar = () => {
+  return (
+    <div>
+      <div className="row">
+        <div className="col-md-4">Footer1</div>
+      </div>
+      <div className="row">
+        <div className="col-md-4">Footer2</div>
+      </div>
+      <div className="row">
+        <div className="col-md-4">Footer3</div>
+      </div>
+      <div className="row">
+        <div className="col-md-4">Footer4</div>
+      </div>
+      <div className="row">
+        <div className="col-md-4">Footer5</div>
+      </div>
+    </div>
+  );
+};
+
+export default navbar;
