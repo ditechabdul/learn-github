ef9e43d302f0fd58ba4fd831702b52913a26db2f
ditechabdul <ditechabdul@gmail.com>
2022-11-21 14:18:52 +0500

    add banner.js file in component

---
+++ component/banner.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const navbar = () => {
+  return (
+    <div>
+      <div className="row">
+        <div className="col-md-4">hello1</div>
+      </div>
+      <div className="row">
+        <div className="col-md-4">hello2</div>
+      </div>
+      <div className="row">
+        <div className="col-md-4">hello3</div>
+      </div>
+      <div className="row">
+        <div className="col-md-4">hello4</div>
+      </div>
+      <div className="row">
+        <div className="col-md-4">hello5</div>
+      </div>
+    </div>
+  );
+};
+
+export default navbar;
